ad85c1448ac368a87e85ff83d72a3f275369cb07
jensbrak <52660827+jensbrak@users.noreply.github.com>
2022-12-22 15:59:17 +0100

    Mistlands: new hairs and use of SharedItemData
    The hairs were previously hard coded (for localization purposes I believe) but I think it's more important to read them from the SharedItemData DB instead, so I did that

--- Loki/Hair.cs
+++ Loki/Hair.cs
@@ -5,25 +5,13 @@ namespace Loki
 {
     public readonly struct Hair
     {
-        private static readonly Hair[] SensibleHairs =
-        {
-            new Hair(Loki.Properties.Resources.No_hair, "HairNone"),
-            new Hair(Loki.Properties.Resources.Braided_1, "Hair3"),
-            new Hair(Loki.Properties.Resources.Braided_2, "Hair11"),
-            new Hair(Loki.Properties.Resources.Braided_3, "Hair12"),
-            new Hair(Loki.Properties.Resources.Braided_4, "Hair6"),
-            new Hair(Loki.Properties.Resources.Long_1, "Hair1"),
-            new Hair(Loki.Properties.Resources.Ponytail_1, "Hair2"),
-            new Hair(Loki.Properties.Resources.Ponytail_2, "Hair4"),
-            new Hair(Loki.Properties.Resources.Ponytail_3, "Hair7"),
-            new Hair(Loki.Properties.Resources.Ponytail_4, "Hair5"),
-            new Hair(Loki.Properties.Resources.Short_1, "Hair8"),
-            new Hair(Loki.Properties.Resources.Short_2, "Hair13"),
-            new Hair(Loki.Properties.Resources.Side_Swept_1, "Hair9"),
-            new Hair(Loki.Properties.Resources.Side_Swept_2, "Hair10"),
-            new Hair(Loki.Properties.Resources.Side_Swept_3, "Hair14"),
-            new Hair(Loki.Properties.Resources.Blob_hair, "TrophyBlob"),
-        };
+        private static readonly IEnumerable<Hair> SensibleHairs =
+            ItemDb.AllItems
+                .Where(i =>
+                    i.ItemType == ItemType.Customization
+                    && i.ItemName.ToLower().Contains("hair"))
+                .Select(i =>
+                    new Hair(i.DisplayName, i.ItemName));
 
         private static readonly IEnumerable<Hair> SillyHairs =
             ItemDb.AllItems.Where(i => i.ItemType == ItemType.Trophy)
